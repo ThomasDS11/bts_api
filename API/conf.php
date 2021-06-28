83360d733cea93a710a5c036b3ce710fec00ac3c
ThomasDS11 <61500301+ThomasDS11@users.noreply.github.com>
2021-06-28 17:41:37 +0200

    Add files via upload

---
+++ API/conf.php
@@ -0,0 +1,4 @@
+<?php
+$connex=new PDO('mysql:host=localhost;dbname=applicationgsb','phpuser','php',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+?>
+<h1><a href="index.html">ACCEUIL</a></h1>
